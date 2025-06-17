69388eaf1814420e97052e79a9bc4cc6829f7445
minoru-yamanaka <minoruyamanaka@icloud.com>
2025-06-17 19:40:34 -0300

    git up

--- 11.2_projeto/produtos/ver.php
+++ 11.2_projeto/produtos/ver.php
@@ -26,7 +26,7 @@ if(!$produto) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 
 <body>
