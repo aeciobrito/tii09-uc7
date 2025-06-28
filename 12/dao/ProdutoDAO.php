7c8e396ccf1a504d9488afe1c17727148b8ab51e
aecio <aeciobrito@gmail.com>
2025-06-27 22:12:21 -0300

    Editar produtos com fornecedores

--- 12/dao/ProdutoDAO.php
+++ 12/dao/ProdutoDAO.php
@@ -55,19 +55,39 @@ class ProdutoDAO
 
     public function getById(int $id): ?Produto
     {
-        $stmt = $this->db->prepare("SELECT * FROM produtos WHERE id = :id");
+        $stmt = $this->db->prepare(
+            "SELECT p.*, 
+            f.id AS fornecedor_id,
+            f.nome AS fornecedor_nome,
+            f.cnpj AS fornecedor_cnpj,
+            f.contato AS fornecedor_contato
+            FROM produtos p 
+            LEFT JOIN fornecedores f
+            ON p.fornecedor_id = f.id WHERE p.id = :id");
         // Para getById, bindParam ainda é uma boa prática para clareza e segurança com o tipo.
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         $stmt->execute();
         $data = $stmt->fetch();
         if ($data) {
+            $fornecedor = null;
+            if(isset($data['fornecedor_id']))
+            {
+                $fornecedor = new Fornecedor(
+                    $data['fornecedor_id'],
+                    $data['fornecedor_nome'],
+                    $data['fornecedor_cnpj'],
+                    $data['fornecedor_contato']
+                );
+            }
+
             return new Produto(
                 $data['id'],
                 $data['nome'],
                 (float)$data['preco'],
                 (bool)$data['ativo'],
                 $data['dataDeCadastro'],
-                $data['dataDeValidade']
+                $data['dataDeValidade'],
+                $fornecedor
             );
         }
         return null;
@@ -92,9 +112,12 @@ class ProdutoDAO
     {
         $sql = "UPDATE produtos 
                 SET nome = :nome, preco = :preco, ativo = :ativo, 
-                    dataDeCadastro = :dataDeCadastro, dataDeValidade = :dataDeValidade 
+                    dataDeCadastro = :dataDeCadastro, dataDeValidade = :dataDeValidade, 
+                    fornecedor_id = :fornecedor_id
                 WHERE id = :id";
         $stmt = $this->db->prepare($sql);
+
+        $fornecedorId = $produto->getFornecedor() ? $produto->getFornecedor()->getId() : null;
 
         return $stmt->execute([
             ':id' => $produto->getId(),
@@ -102,7 +125,8 @@ class ProdutoDAO
             ':preco' => $produto->getPreco(),
             ':ativo' => $produto->getAtivo() ? 1 : 0,
             ':dataDeCadastro' => $produto->getDataDeCadastro(),
-            ':dataDeValidade' => $produto->getDataDeValidade()
+            ':dataDeValidade' => $produto->getDataDeValidade(),
+            ':fornecedor_id' => $fornecedorId
         ]);
     }
 
